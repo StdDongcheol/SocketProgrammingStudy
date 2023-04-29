2b68a102c86387cb2bd5873af64fe4b408e152d8
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-29 21:57:45 +0900

    feat: TCP based Echo, Client create.

---
+++ TCPEchoClient/TCPEchoClient.cpp
@@ -0,0 +1,69 @@
+﻿
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUF_SIZE        1024
+#define SERVER_PORT        9591
+
+int main()
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN ServerAddr;
+	
+	char ServerIP[16];
+	std::string strMessage;
+	char Message[BUF_SIZE];
+	int strLength;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NULL)
+	{
+		std::cout << "Error!!!";
+		exit(1);
+	}
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		exit(1);
+
+	std::cout << "input to Connect server IP : ";
+	std::cin >> ServerIP;
+
+	if (!ServerIP)
+		exit(1);
+
+	memset(&ServerAddr, 0, sizeof(ServerAddr));
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_addr.S_un.S_addr = inet_addr(ServerIP); // 127.0.0.1
+	ServerAddr.sin_port = htons(SERVER_PORT);	// port 9591
+
+	if (connect(hSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
+		exit(1);
+
+	std::cout << "Server Connected !!!!\n";
+	std::cout << "================================================\n";
+
+	while (true)
+	{
+		std::cout << "input your message. (0 will quit.) : ";
+		std::cin >> strMessage;
+
+		if (strMessage[0] == '0')
+			break;
+
+		send(hSocket, strMessage.c_str(), (int)strMessage.size(), 0);
+		strLength = recv(hSocket, Message, BUF_SIZE - 1, 0);
+		Message[strLength] = 0;
+
+		std::cout << "Recieved message from Server : " << Message << '\n';
+	}
+
+	closesocket(hSocket);
+	WSACleanup();
+
+	return 0;
+}
---
+++ TCPEchoServer/TCPEchoServer.cpp
@@ -0,0 +1,74 @@
+﻿
+
+#include <iostream>
+#include <stdlib.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUF_SIZE 1024
+#define SERVER_PORT        9591
+
+int main(int args, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServerSock, hClientSocket;
+	SOCKADDR_IN ServerAddr, ClientAddr;
+	int ClientAddrSize;
+
+	char Message[BUF_SIZE] = {};
+	int strLength;
+
+	args = 2;
+	if (args != 2)
+	{
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NULL)
+	{
+		std::cout << "Error!!!";
+		exit(1);
+	}
+
+	hServerSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServerSock == INVALID_SOCKET)
+		exit(1);
+
+	//int opt = 1;
+	//setsockopt(hServerSock, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt));
+
+	memset(&ServerAddr, 0, sizeof(ServerAddr));
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	ServerAddr.sin_port = htons(SERVER_PORT);
+
+	bind(hServerSock, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
+
+	if (listen(hServerSock, 5) == SOCKET_ERROR)
+		exit(1);
+
+	ClientAddrSize = sizeof(ClientAddr);
+	
+	
+	for (int i = 0; i < 5; ++i)
+	{
+		hClientSocket = accept(hServerSock, (SOCKADDR*)&ClientAddr, &ClientAddrSize);
+		if (hClientSocket == INVALID_SOCKET)
+			exit(1);
+
+		std::cout << "Connected client!! " << i + 1 << "\n";
+
+		while ((strLength = recv(hClientSocket, Message, BUF_SIZE, 0)) != 0)
+		{
+			send(hClientSocket, Message, strLength, 0);
+		}
+
+		closesocket(hClientSocket);
+	}
+
+	closesocket(hServerSock);
+	WSACleanup();
+
+	return 0;
+}
