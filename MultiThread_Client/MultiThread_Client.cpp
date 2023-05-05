988da977d8ab800002c73ced18af1d1b4b944052
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-05 21:21:10 +0900

    feat: Multithread Server, Client created

---
+++ MultiThread_Client/MultiThread_Client.cpp
@@ -0,0 +1,103 @@
+﻿// MultiThread_Client.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <process.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+char msg[BUF_SIZE];
+char Nickname[NAME_SIZE] = "[UNKNOWN]";
+
+unsigned WINAPI SendMsgFunc(void* arg)
+{
+    SOCKET hSock = (SOCKET)arg;
+    char NameMsg[NAME_SIZE + BUF_SIZE];
+
+    while (true) 
+    {
+        std::cin.getline(msg, BUF_SIZE);
+
+        if (msg[0] == '^' && msg[1] == 'Z')
+        {
+            closesocket(hSock);
+            exit(0);
+        }
+        sprintf_s(NameMsg, "%s %s", Nickname, msg);
+       
+        send(hSock, NameMsg, (int)strlen(NameMsg), 0);
+    }
+
+    return 0;
+}
+
+unsigned WINAPI RecvMsgFunc(void* arg)
+{
+    SOCKET hSock = (SOCKET)arg;
+
+    char NameMsg[NAME_SIZE + BUF_SIZE];
+
+    int strLength = 0;
+
+    while (true)
+    {
+        strLength = recv(hSock, NameMsg, NAME_SIZE + BUF_SIZE - 1, 0);
+
+        if (strLength == -1)
+            return -1;
+
+        NameMsg[strLength] = 0;
+
+        std::cout << NameMsg << std::endl;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    WSADATA wsaData;
+    SOCKET hMySocket;
+    SOCKADDR_IN serverAddress;
+    HANDLE hSendThread, hRecvThread;
+
+    std::string strServerIP;
+    int ServerPort = -1;
+
+    std::cout << "input your name :";
+    std::cin >> Nickname;
+    std::cout << "input connect to serverIP :";
+    std::cin >> strServerIP;
+    std::cout << "input connect to Port :";
+    std::cin >> ServerPort;
+
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    
+    hMySocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&serverAddress, 0, sizeof(SOCKADDR_IN));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.S_un.S_addr = inet_addr(strServerIP.c_str());
+    serverAddress.sin_port = htons(ServerPort);
+
+    if (SOCKET_ERROR == connect(hMySocket, (sockaddr*)&serverAddress, sizeof(serverAddress)))
+    {
+        std::cout << "Error!!!!!!!!";
+        exit(-1);
+    }
+    
+    hSendThread = (HANDLE)_beginthreadex(NULL, 0, SendMsgFunc, (void*)hMySocket, 0, NULL);
+    hRecvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsgFunc, (void*)hMySocket, 0, NULL);
+
+    WaitForSingleObject(hSendThread, INFINITE);
+    WaitForSingleObject(hRecvThread, INFINITE);
+
+    closesocket(hMySocket);
+    WSACleanup();
+    return 0;
+}
---
+++ MultiThread_Server/MultiThread_Server.cpp
@@ -0,0 +1,111 @@
+﻿// MultiThread_Server.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <process.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+#define SERVER_PORT 30928
+
+int ClientCount = 0;
+SOCKET clientSockets[MAX_CLNT];
+HANDLE hMutex;
+
+void SendMessageClient(char* msg, int length)
+{
+    WaitForSingleObject(hMutex, INFINITE);
+
+    for (int i = 0; i < ClientCount; ++i)
+        send(clientSockets[i], msg, length, 0);
+
+    ReleaseMutex(hMutex);
+}
+
+unsigned WINAPI HandleClient(void* arg)
+{
+    SOCKET hClientSocket = (SOCKET)arg;
+    int strLength = 0;
+    char msg[BUF_SIZE];
+    
+    while ((strLength = recv(hClientSocket, msg, sizeof(msg), 0)) != 0)
+    {
+        SendMessageClient(msg, strLength);
+    }
+
+    WaitForSingleObject(hMutex, INFINITE);
+
+    // 연결이 끊겨있는 클라이언트 제거
+    for (int i = 0; i < ClientCount; ++i)
+    {
+        if (clientSockets[i] == hClientSocket)
+        {
+            // 이미 끊겨있는 대상 소켓에 하나씩 소켓 밀기 
+            while (i < ClientCount - 1)
+            {
+                ++i;
+                clientSockets[i] = clientSockets[i + 1];
+            }
+            break;
+        }
+    }
+
+    --ClientCount;
+    ReleaseMutex(hMutex);
+    closesocket(hClientSocket);
+    return 0;
+}
+
+int main()
+{
+    WSADATA wsaData;
+    SOCKET hServerSocket, hClientSocket;
+    SOCKADDR_IN serverAddress, clientAddress;
+    HANDLE hThread;
+
+    int ClientAddressSize = 0;
+
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    hMutex = CreateMutex(NULL, FALSE, NULL);
+    hServerSocket = socket(PF_INET, SOCK_STREAM, NULL);
+
+    memset(&serverAddress, 0, sizeof(SOCKADDR_IN));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(SERVER_PORT);
+
+    bind(hServerSocket, (sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    listen(hServerSocket, 5);
+
+    std::cout << "Current server IP : " << inet_ntoa(serverAddress.sin_addr) << "\n";
+    
+    while (true)
+    {
+        ClientAddressSize = sizeof(clientAddress);
+        hClientSocket = accept(hServerSocket, (sockaddr*)&clientAddress, &ClientAddressSize);
+
+        WaitForSingleObject(hMutex, INFINITE);
+
+        clientSockets[ClientCount] = hClientSocket;     // CLIENTARR에 CLIENT 등록
+        ++ClientCount;
+
+
+        ReleaseMutex(hMutex);
+
+        hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClient, (void*)hClientSocket, 0, NULL);
+
+        std::cout << "Client Entered! (IP : " << inet_ntoa(clientAddress.sin_addr) << ")\n";
+    }
+
+    closesocket(hServerSocket);
+    WSACleanup();
+
+    return 0;
+}
